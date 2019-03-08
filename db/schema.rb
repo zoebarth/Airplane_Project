a2bbcdf74a6a875125ba5464c3263157d55a8d88
Zoe Barth <zoe.barth@gmail.com>
2019-03-08 10:50:06 -0500

    new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_033535) do
+ActiveRecord::Schema.define(version: 2019_03_08_154835) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "trips", force: :cascade do |t|
     t.date "arrive_date"
